8035c78a42727c10d6ae29486080cb65380f6f58
walid younis <walid.younis.90@gmail.com>
2020-08-23 18:14:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,14 +6,14 @@ data.each_with_object({}) do |(k,v),final_array|
   if v ==:grey
 v.each do |names,key|
   key.each do |name|
-     
+  end 
   if !final_array[name] 
     final_array[name] = {}
     if !final_array[name][k] 
     final_array[name][k] = []
   end
  end
- end 
+
 
 
  final_array[name][k].push(names.to_s)
